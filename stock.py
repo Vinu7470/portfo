04c87d12f69f4d39245341baf70feb1f6eaa1a27
Vinu7470 <vinu.vijayan24487@gmail.com>
2025-07-29 09:32:12 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -4,7 +4,7 @@ from datetime import date
 
 import yfinance as yf
 from prophet import Prophet
-from fbprophet.plot import plot_plotly
+from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 START = "2015-01-01"
